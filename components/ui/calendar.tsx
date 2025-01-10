5d436146e8e72fe4ffda552b3a25218eccc15656
aaetkiwi <aaetmati@gmail.com>
2025-01-10 11:35:04 +0000

    final fix

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -1,7 +1,6 @@
 import * as React from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
 import { DayPicker } from "react-day-picker"
-
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
 
@@ -52,8 +51,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconNext: ({ ...props }) => <ChevronRight className="h-4 w-4" {...props} />,
-        IconPrev: ({ ...props }) => <ChevronLeft className="h-4 w-4" {...props} />,
+        PrevButton: (props) => <ChevronLeft className="h-4 w-4" {...props} />,
+        NextButton: (props) => <ChevronRight className="h-4 w-4" {...props} />,
       }}
       {...props}
     />
